1483dc9e80b376b4ee411ad1af9df17eb3b40afe
KrisnahadiJ <krisnahadij@gmail.com>
2024-11-23 21:41:52 +0700

    Percobaan 1: Membuat Fungsi Tanpa Parameter

---
+++ Pertemuan13/Percobaan11.java
@@ -0,0 +1,33 @@
+package Pertemuan13; //DENGAN FUNGSI
+import java.util.Scanner;
+
+public class Percobaan11 {
+
+    static int hitungLuas(int p, int l) {
+        return p * l;
+    }
+
+    static int hitungVolume(int t, int p, int l) {
+        return hitungLuas(p, l) * t;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p, l, t;
+
+        System.out.print("Masukkan panjang: ");
+        p = input.nextInt();
+
+        System.out.print("Masukkan lebar: ");
+        l = input.nextInt();
+
+        System.out.print("Masukkan tinggi: ");
+        t = input.nextInt();
+
+        int luas = hitungLuas(p, l);
+        System.out.println("Luas Persegi Panjang adalah: " + luas);
+
+        int volume = hitungVolume(t, p, l);
+        System.out.println("Volume Balok adalah: " + volume);
+    }
+}
