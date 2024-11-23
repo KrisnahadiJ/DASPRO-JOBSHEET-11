6b8046ebd4a327eab5053d77c1e637c12025667e
KrisnahadiJ <krisnahadij@gmail.com>
2024-11-23 19:10:29 +0700

    Modifikasi code tanpa fungsi Menu

--- Pertemuan13/Kafe11.java
+++ Pertemuan13/Kafe11.java
@@ -1,7 +1,7 @@
 package Pertemuan13;
 
 public class Kafe11 {
-    public static void Menu() {
+    public static void main(String[] args) {
         System.out.println("====== MENU RESTO KAFE ======");
         System.out.println("1. Kopi Hitam    - Rp 15,000");
         System.out.println("2. Cappuccino    - Rp 20,000");
@@ -11,9 +11,5 @@ public class Kafe11 {
         System.out.println("6. Mie Goreng    - Rp 18,000");
         System.out.println("=============================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
-
-    public static void main(String[] args) {
-        Menu();
     }
 }
