7b71f58f63c14eeab7fbd54f2d25b99e7baf8220
KrisnahadiJ <krisnahadij@gmail.com>
2024-11-23 20:14:57 +0700

    Percobaan 3: Modifikasi sehingga fungsi hitungTotal dapat menerima kode Promo

--- Pertemuan13/Kafe11.java
+++ Pertemuan13/Kafe11.java
@@ -1,4 +1,5 @@
 package Pertemuan13;
+import java.util.Scanner;
 
 public class Kafe11 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
@@ -6,6 +7,14 @@ public class Kafe11 {
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Berikan diskon 50%.");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Berikan diskon 30%.");
+        } else {
+            System.out.println("Kode promo tidak valid.");
         }
 
         System.out.println("===== MENU RESTO KAFE =====");
@@ -16,19 +25,37 @@ public class Kafe11 {
         System.out.println("5. Roti Bakar    - Rp 10,000");
         System.out.println("6. Mie Goreng    - Rp 18,000");
         System.out.println("===========================");
-
-        if (kodePromo.equals("DISKON50")) {
-            System.out.println("Selamat! Anda mendapatkan diskon 50%.");
-        } else if (kodePromo.equals("DISKON30")) {
-            System.out.println("Selamat! Anda mendapatkan diskon 30%.");
-        } else {
-            System.out.println("Kode promo tidak valid.");
-        }
-
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
 
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem, String kodePromo) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+    
+        if (kodePromo.equals("DISKON50")) {
+            hargaTotal = hargaTotal / 2; // Diskon 50%
+        } else if (kodePromo.equals("DISKON30")) {
+            hargaTotal = hargaTotal * 70 / 100; // Diskon 30%
+        } else {
+            System.out.println("Kode promo tidak valid. Tidak ada pengurangan harga.");
+        }
+    
+        return hargaTotal;
+    }
+    
+
     public static void main(String[] args) {
         Menu("Andi", true, "DISKON50");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, "DISKON50");
+
+        System.out.println("Total harga untuk pesanan Anda: Rp " + totalHarga);
     }
 }
